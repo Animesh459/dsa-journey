b12ba55d51bac6ec92135853a5631e5bb11ead79
Animesh Shuvro <animeshshuvro@gmail.com>
2025-07-24 21:36:31 +0600

    init

---
+++ codeforces/Longwords.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+for _ in range(n):
+    word = input()
+    print(word)
+    if len(word) > 10:
+        abbreviation = word[0] + str(len(word) - 2) + word[-1]
+        print(abbreviation)
+    else:
+        print(word)
+
+
+
