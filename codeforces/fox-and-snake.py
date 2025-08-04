4c4f76056967241473525581f5cbd611c45deefc
Animesh Shuvro <animeshshuvro@gmail.com>
2025-08-04 22:54:21 +0600

    fox and snake

---
+++ codeforces/fox-and-snake.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+
+for i in range(n):
+    if i % 2 == 0:
+        # Odd-numbered row (1-based): full line of #
+        print('#' * m)
+    elif i % 4 == 1:
+        # Even-numbered row after right-moving line: # is at the end
+        print('.' * (m - 1) + '#')
+    else:
+        # Even-numbered row after left-moving line: # is at the start
+        print('#' + '.' * (m - 1))
