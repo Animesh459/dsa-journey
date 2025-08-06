bd8244648bfd9b09a598bc781feca0bf3531c5fb
Animesh Shuvro <animeshshuvro@gmail.com>
2025-08-06 22:15:21 +0600

    chossing teams with i/o

--- codeforces/choosing-teams.py
+++ codeforces/choosing-teams.py
@@ -12,3 +12,24 @@ for p in participations:
 max_teams = eligible_students // 3
 
 print(max_teams)
+
+# Input
+# 5 2
+# 0 4 5 1 0
+#
+# Output
+# 1
+#
+# Input
+# 6 4
+# 0 1 2 3 4 5
+#
+# Output
+# 0
+#
+# Input
+# 6 5
+# 0 0 0 0 0 0
+#
+# Output
+# 2
