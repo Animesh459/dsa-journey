9f9851c34a00a82b5096a7792d12238caec0cfbf
Animesh Shuvro <animeshshuvro@gmail.com>
2025-08-08 22:52:37 +0600

    with i/o

--- codeforces/lunch-rush.py
+++ codeforces/lunch-rush.py
@@ -13,3 +13,29 @@ for _ in range(n):
     max_joy = max(max_joy, joy)  # Keep track of maximum joy
 
 print(max_joy)
+
+#
+# Input
+# 2 5
+# 3 3
+# 4 5
+#
+# Output
+# 4
+#
+# Input
+# 4 6
+# 5 8
+# 3 6
+# 2 3
+# 2 2
+#
+# Output
+# 3
+#
+# Input
+# 1 5
+# 1 7
+#
+# Output
+# -1
