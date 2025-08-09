1423a62a48f17d1d5156379078c8c30451acf2e3
Animesh Shuvro <animeshshuvro@gmail.com>
2025-08-09 22:57:42 +0600

    with i/o

--- codeforces/soft-drinking.py
+++ codeforces/soft-drinking.py
@@ -8,3 +8,22 @@ toasts_by_salt = p // np
 
 result = min(toasts_by_drink, toasts_by_limes, toasts_by_salt) // n
 print(result)
+
+
+# Input
+# 3 4 5 10 8 100 3 1
+#
+# Output
+# 2
+#
+# Input
+# 5 100 10 1 19 90 4 3
+#
+# Output
+# 3
+#
+# Input
+# 10 1000 1000 25 23 1 50 1
+#
+# Output
+# 0
