19de5703c12907e0bd8165b17c12000824c8e6c8
Animesh Shuvro <animeshshuvro@gmail.com>
2025-08-27 21:57:32 +0600

    football

---
+++ codeforces/football.py
@@ -0,0 +1,19 @@
+n = int(input())
+team1 = ""
+team1_count = 0
+team2 = ""
+team2_count = 0
+for i in range(n):
+    goal = input()
+    if team1 == "":
+        team1 = goal
+        team1_count += 1
+    elif goal == team1:
+        team1_count += 1
+    else:
+        team2 = goal
+        team2_count += 1
+if team1_count > team2_count:
+    print(team1)
+else:
+    print(team2)
