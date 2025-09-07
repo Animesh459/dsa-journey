b47d47d14db9b633bb981e4de4aeb65752c212d4
Animesh Shuvro <animeshshuvro@gmail.com>
2025-09-07 22:13:09 +0600

    with i/o

--- codeforces/expression.py
+++ codeforces/expression.py
@@ -9,3 +9,21 @@ result4 = (a + b) * c
 result5 = a + b + c
 
 print(max(result1, result2, result3, result4, result5))
+
+# Examples
+#
+# Input
+# 1
+# 2
+# 3
+#
+# Output
+# 9
+#
+# Input
+# 2
+# 10
+# 3
+#
+# Output
+# 60
