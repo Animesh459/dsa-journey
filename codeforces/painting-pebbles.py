f2850fe65cd08e5d6d309ee8314f41def0ae4aeb
Animesh Shuvro <animeshshuvro@gmail.com>
2025-09-10 23:32:20 +0600

    with i/o

--- codeforces/painting-pebbles.py
+++ codeforces/painting-pebbles.py
@@ -15,3 +15,37 @@ else:
         for j in range(a[i] - min_pebbles):
             colors.append( (j % k) + 1 )
         print(*colors)
+
+#
+# Examples
+#
+# Input
+# 4 4
+# 1 2 3 4
+#
+# Output
+# YES
+#
+# 1
+# 1 4
+# 1 2 4
+# 1 2 3 4
+#
+# Input
+# 5 2
+# 3 2 4 1 3
+#
+# Output
+# NO
+#
+# Input
+# 5 4
+# 3 2 4 3 5
+#
+# Output
+# YES
+# 1 2 3
+# 1 3
+# 1 2 3 4
+# 1 3 4
+# 1 1 2 3 4
