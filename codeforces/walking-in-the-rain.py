620eb317736a794eb608eaba5d5faa0066f4a630
Animesh Shuvro <animeshshuvro@gmail.com>
2025-10-03 10:16:45 +0600

    with i/o

--- codeforces/walking-in-the-rain.py
+++ codeforces/walking-in-the-rain.py
@@ -5,3 +5,19 @@ a = list(map(int, sys.stdin.readline().split()))
 
 ans = min(max(a[i], a[i+1]) for i in range(n-1))
 print(ans)
+
+Examples
+
+Input
+4
+10 3 5 10
+
+Output
+5
+
+Input
+5
+10 2 8 3 5
+
+Output
+5
