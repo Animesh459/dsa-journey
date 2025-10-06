6dd55e332585915f5734a7ce2d2193f39de72ead
Animesh Shuvro <animeshshuvro@gmail.com>
2025-10-06 20:50:48 +0600

    counting rhombi

---
+++ codeforces/counting-rhombi.py
@@ -0,0 +1,10 @@
+def count_rhombi(w, h):
+    total = 0
+    for dx in range(1, w // 2 + 1):
+        for dy in range(1, h // 2 + 1):
+            total += (w - 2 * dx + 1) * (h - 2 * dy + 1)
+    return total
+
+# Input
+w, h = map(int, input().split())
+print(count_rhombi(w, h))
