6dd1b7ddff8b608cbafd78b43d34432a2367ac2f
Animesh Shuvro <animeshshuvro@gmail.com>
2026-01-01 23:09:07 +0600

    ladder

---
+++ codeforces/ladder.py
@@ -0,0 +1,41 @@
+import sys
+
+def main():
+    input = sys.stdin.readline
+
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+
+    # up[i]: farthest index reachable from i with non-decreasing sequence
+    up = [0] * n
+    up[-1] = n - 1
+    for i in range(n - 2, -1, -1):
+        if a[i] <= a[i + 1]:
+            up[i] = up[i + 1]
+        else:
+            up[i] = i
+
+    # down[i]: farthest index reachable from i with non-increasing sequence
+    down = [0] * n
+    down[-1] = n - 1
+    for i in range(n - 2, -1, -1):
+        if a[i] >= a[i + 1]:
+            down[i] = down[i + 1]
+        else:
+            down[i] = i
+
+    out = []
+    for _ in range(m):
+        l, r = map(int, input().split())
+        l -= 1
+        r -= 1
+
+        if down[up[l]] >= r:
+            out.append("Yes")
+        else:
+            out.append("No")
+
+    print("\n".join(out))
+
+if __name__ == "__main__":
+    main()
