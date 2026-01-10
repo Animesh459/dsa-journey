aced29b5c35fac30e8c5e3e6967b74f295fc1549
Animesh Shuvro <animeshshuvro@gmail.com>
2026-01-10 19:54:45 +0600

    i/o out put

--- codeforces/lcm-challenge.py
+++ codeforces/lcm-challenge.py
@@ -16,3 +16,18 @@ def max_lcm_of_three(n: int) -> int:
 if __name__ == "__main__":
     n = int(input().strip())
     print(max_lcm_of_three(n))
+
+
+# Examples
+#
+# Input
+# 9
+#
+# Output
+# 504
+#
+# Input
+# 7
+#
+# Output
+# 210
