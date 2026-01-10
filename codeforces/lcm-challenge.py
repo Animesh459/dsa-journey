719b34b17318f0423ba9e3d79c4f9ee48f732bb6
Animesh Shuvro <animeshshuvro@gmail.com>
2026-01-10 19:53:10 +0600

    solved

---
+++ codeforces/lcm-challenge.py
@@ -0,0 +1,18 @@
+def max_lcm_of_three(n: int) -> int:
+    if n <= 2:
+        return n
+
+    if n % 2 == 1:
+        # n is odd
+        return n * (n - 1) * (n - 2)
+    else:
+        # n is even
+        if n % 3 != 0:
+            return n * (n - 1) * (n - 3)
+        else:
+            return (n - 1) * (n - 2) * (n - 3)
+
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    print(max_lcm_of_three(n))
