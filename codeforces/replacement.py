de4fda1504cf3cf05914afa1f0a772f85dbe64db
Animesh Shuvro <animeshshuvro@gmail.com>
2026-01-17 20:34:08 +0600

    example i/o added

--- codeforces/replacement.py
+++ codeforces/replacement.py
@@ -14,3 +14,20 @@ def solve():
 
 if __name__ == "__main__":
     solve()
+
+Examples
+InputCopy
+5
+1 2 3 4 5
+OutputCopy
+1 1 2 3 4
+InputCopy
+5
+2 3 4 5 6
+OutputCopy
+1 2 3 4 5
+InputCopy
+3
+2 2 2
+OutputCopy
+1 2 2
