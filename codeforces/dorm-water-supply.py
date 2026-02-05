c837b3f245931eb713a957b5ecd08b9b547380ff
Animesh Shuvro <animeshshuvro@gmail.com>
2026-02-05 19:35:43 +0600

    dorm water supply solution added

---
+++ codeforces/dorm-water-supply.py
@@ -0,0 +1,35 @@
+def solve():
+    n, p = map(int, input().split())
+
+    outgoing = [None] * (n + 1)
+    indeg = [0] * (n + 1)
+    outdeg = [0] * (n + 1)
+
+    for _ in range(p):
+        a, b, d = map(int, input().split())
+        outgoing[a] = (b, d)
+        outdeg[a] += 1
+        indeg[b] += 1
+
+    res = []
+
+    for i in range(1, n + 1):
+        # tank: has outgoing but no incoming
+        if outdeg[i] == 1 and indeg[i] == 0:
+            cur = i
+            min_diam = 10**18
+
+            while outgoing[cur] is not None:
+                nxt, diam = outgoing[cur]
+                min_diam = min(min_diam, diam)
+                cur = nxt
+
+            res.append((i, cur, min_diam))
+
+    print(len(res))
+    for a, b, d in res:
+        print(a, b, d)
+
+
+if __name__ == "__main__":
+    solve()
