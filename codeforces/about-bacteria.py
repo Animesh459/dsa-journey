b5465db9a59e5a159ea2c47a719ac2e44bab28d8
Animesh Shuvro <animeshshuvro@gmail.com>
2026-02-10 19:55:00 +0600

    example i/o refactor

--- codeforces/about-bacteria.py
+++ codeforces/about-bacteria.py
@@ -32,16 +32,22 @@ if __name__ == "__main__":
     main()
 
 
-Examples
-InputCopy
-3 1 3 5
-OutputCopy
-2
-InputCopy
-1 4 4 7
-OutputCopy
-3
-InputCopy
-2 2 4 100
-OutputCopy
-0
+# Examples
+
+# Input
+# 3 1 3 5
+
+# Output
+# 2
+
+# Input
+# 1 4 4 7
+
+# Output
+# 3
+
+# Input
+# 2 2 4 100
+
+# Output
+# 0
